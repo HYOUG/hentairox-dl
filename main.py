91af02640cce5a4254a33089715a92f160f82f05
HYOUG <hyoug99931@gmail.com>
2021-08-13 17:23:33 +0200

    Upgraded log messages and error handling

--- main.py
+++ main.py
@@ -27,23 +27,26 @@ def dl_gallery(target_url: str) -> None:
     gallery_id = list(filter(check_int, target_url.split("/")))[0]
 
     with session() as s:
-        print("Getting number of pages...")
+        print("Getting number of pages")
         response = s.get(target_url)
         soup = BeautifulSoup(response.content, "html.parser")
         page_node = soup.find("li", {"class": "pages"})
         page_num = int(page_node.contents[0].split(" ")[0])
         
-        print(f"Getting URL pattern...")
+        print(f"Getting URL pattern")
         page_url = f"{target_url}/1".replace("gallery", "view")
         response = s.get(page_url)
         soup = BeautifulSoup(response.content, "html.parser")
         pattern = "/".join(soup.find(id="gimg")["src"].split("/")[:-1])
         
-        print(f"Generating new folder : .downloads/{gallery_id}/")
-        mkdir(f"./downloads/{gallery_id}")
+        try:
+            print(f"Generating new folder : .downloads/{gallery_id}/")
+            mkdir(f"./downloads/{gallery_id}")
+        except FileExistsError:
+            pass
 
         for i in range(1, page_num+1):
-            print(f"Downloading page n°{i} /{page_num}...")
+            print(f"Downloading page n° {i}/{page_num}")
             response = s.get(f"{pattern}/{i}.jpg")
             im = open(f"./downloads/{gallery_id}/{gallery_id}_{i}.jpg", "wb")
             im.write(response.content)
